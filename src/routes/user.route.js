a171a655e80d0ebbb2398e965b3ad49bc171799b
MahmoudBakrey <mahmoud.ae.bakry@gmail.com>
2018-03-25 13:41:41 -0700

    add carton of one provider

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,6 @@
 import UserController from '../controllers/user.controller'
 import galonController from '../controllers/galon/galon.controller'
+import cartonController from '../controllers/cartona/cartona.controller'
 import express from 'express';
 import passport from "passport";
 import passportService from '../services/passport';
@@ -20,7 +21,7 @@ router.route("/users/:userId/galons")
     .get(galonController.galonsOfOneProvider)
 
 router.route("/users/:userId/cartons")
-    .get(galonController.galonsOfOneProvider)
+    .get(cartonController.cartonsOfOneProvider)
 export default router;
 
 
