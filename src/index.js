911e0535ef4e69d6506b1f06ef9373b0ed400bfb
MahmoudBakrey <mahmoud.ae.bakry@gmail.com>
2018-03-27 07:30:16 -0700

    sa

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "babel-polyfill";
 import app from './app';
 
-let Port = process.env.PORT || 3000;
+let Port = process.env.PORT || 8888;
 
 app.listen(Port, () => {
     console.log('server is running now in port... ' + Port)
